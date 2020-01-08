72c81ee51dc724766725fa98857bba388640e45a
Real <melissa.real@hotmail.com>
2020-01-08 18:14:58 +0000

    update player hooks

---
+++ src/components/Heroes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Heroes extends Component {
+    render() {
+        return (
+            <div>
+                   <form>
+           
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Heroes;
+
