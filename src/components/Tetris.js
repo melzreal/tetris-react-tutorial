36e31498d6726dc0729b65155d4dcb4a49de7450
Real <melissa.real@hotmail.com>
2020-01-09 23:32:36 +0000

    sweeprows

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
-import {createStage} from '../gameHelpers';
-
+import {createStage, checkCollision} from '../gameHelpers';
 
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
-
 //custom Hooks
-import { usePlayer, updatePlayerPos } from '../hooks/usePlayer';
+import { usePlayer, resetPlayer, updatePlayerPos } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
 
@@ -17,23 +15,35 @@ const Tetris = () => {
 	const[dropTime, setDropTime] = useState(null);
 	const[gameOver, setGameOver] = useState(false);
 	
-	const[player, updatePlayerPos, resetPlayer] = usePlayer();
+	const[player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
 	const[stage, setStage] = useStage(player);
 
 	
 	console.log('re-render');
 
 	const movePlayer = dir => {
+		if(!checkCollision(player, stage, { x: dir, y: 0  })) {
 		updatePlayerPos({ x: dir, y: 0})
+		}
 	}
 
 	const startGame = () => {
 		setStage(createStage());
 		resetPlayer();
+		setGameOver(false);
 	}
 
 	const drop = () => {
+		if(!checkCollision(player, stage, { x: 0, y: 1  })) {
 		updatePlayerPos({ x: 0, y: 1, collided: false});
+		} else {
+			if(player.pos.y < 1){
+				console.log("Game over")
+				setGameOver(true);
+				setDropTime(null);
+			}
+			updatePlayerPos({ x: 0, y: 0, collided: true });
+		}
 	}
 
 	const dropPlayer = () => {
@@ -49,6 +59,8 @@ const Tetris = () => {
 				movePlayer(1);
 			} else if(keyCode == 40){
 				dropPlayer();
+			} else if(keyCode == 38){
+				playerRotate(stage, 1);
 			}
 		}
 	}
@@ -70,7 +82,7 @@ const Tetris = () => {
 				</div>
 				)}
 				
-				<StartButton onClick={startGame}/>
+				<StartButton callback={startGame}/>
 			</aside>
 		
 			</StyledTetris>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,34 +1,59 @@
 import { useState, useCallback} from 'react';
-import { randomTetromino} from '../tetrominos';
-import { STAGE_WIDTH } from '../gameHelpers';
+import { TETROMINOS, randomTetromino} from '../tetrominos';
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 //pos - player position 
 
 export const usePlayer = () => {
   
   const[player, setPlayer] = useState({
-
     pos: { x: 0, y: 0},
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     collided: false,
   });
 
+  const rotate = (matrix, dir) => {
+    //turn rows into collumns
+    const rotatedTetro = matrix.map((_, index) => matrix.map(col=> col[index]));
+    //reverse each row to get a rotated matrix
+    if (dir > 0 ) return rotatedTetro.map(row => row.reverse());
+    return rotatedTetro.reverse();
+  }
+
+  const playerRotate = (stage, dir) => {
+    //making a complete copy of the player so we dont mutate state
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while(checkCollision(clonedPlayer, stage, { x: 0, y: 0 })){
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length){
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+    setPlayer(clonedPlayer);
+  };
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer( prev => ({
       ...prev,
       pos: { x:(prev.pos.x += x), y: (prev.pos.y += y)},
       collided,
-    }))
-  }
+    }));
+  };
 
-  const resetPlayer = useCallback( () =>{
+  const resetPlayer = useCallback(() => {
     setPlayer({
       pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
       tetromino: randomTetromino().shape,
       collided: false,
-    })
-    }, [])
+    });
+    }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 
-}
+};
